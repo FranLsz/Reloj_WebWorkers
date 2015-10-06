f66815d0553b875aa11997954fd93e6b80e02a05
FranLsz <franlsz95develop@gmail.com>
2015-10-06 14:21:26 +0200

    add first code

---
+++ WebWorkers/Scripts/app.js
@@ -0,0 +1,51 @@
+﻿//EJERCICIO: añadir un reloj hh:mm:ss con los botones de cambiar hora
+// (suma +1 o -1 para reflejar hora peninsular o insular), cambiar color (al iniciar aleatoriamente pondra un color y los botones de parar e iniciar la hora.
+
+
+//instanciamos el worker del temporzador
+var worker = new Worker("Scripts/worker_hora.js");
+
+function lanzarWorker() {
+    var worker = new Worker("Scripts/worker.js");
+
+    worker.onmessage = function (evt) {
+
+        document.getElementById("seg").innerHTML = evt.data;
+
+        if (evt.data == "0") {
+
+            location.reload();
+
+        }
+    }
+}
+
+
+worker.onmessage = function (evt) {
+
+    document.getElementById("seg").innerHTML = evt.data;
+
+    if (evt.data == "0") {
+
+        location.reload();
+
+    }
+}
+
+function parar() {
+    worker.terminate();
+    worker = undefined;
+}
+
+function reiniciar() {
+    if (worker == undefined) 
+        lanzarWorker();
+    else
+        worker.postMessage("reiniciar");
+
+
+    }
+
+
+    document.getElementById("stop").onclick = parar;
+    document.getElementById("reiniciar").onclick = reiniciar;
---
+++ WebWorkers/Scripts/worker_hora.js
@@ -0,0 +1,22 @@
+﻿//WEB WORKER-> temporizador 
+
+var segundos = 60;
+
+
+function bajar() {
+    segundos--;
+
+    self.postMessage(segundos);
+
+    //ejecuta la funcion cada segundo
+    setTimeout("bajar()", 1000);
+
+}
+
+self.onmessage = function (evt) {
+
+    if (evt.data == "reiniciar")
+        segundos = 60;
+}
+
+bajar();
